51a1d473acd2369b0ef0b6929b978dab32797223
王冬阳 <wangdongyang@bytedance.com>
2018-03-27 18:28:59 +0800

    initial commit
    Change-Id: I686951d675d77d357a6611c2429b13be082d61d7

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "extends": "airbnb-base",
+    "env": {
+        "browser": true,
+    },
+    "globals": {
+        "window": true,
+        "navigator": true
+    },
+    "rules": {
+        "eqeqeq": "off",
+    }
+};
